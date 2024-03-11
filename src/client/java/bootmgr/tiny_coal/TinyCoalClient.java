0ca307e9c651c9c43debe3ff3a3229d4ed302ef8
CHenHaoYan1234 <chanhaoyan@126.com>
2024-03-11 17:05:28 +0800

    first commit

---
+++ src/client/java/bootmgr/tiny_coal/TinyCoalClient.java
@@ -0,0 +1,10 @@
+package bootmgr.tiny_coal;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TinyCoalClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
